851d92aa77d8ae2ade5d6ceedd57e34f5156b463
Ralph Schaer <ralphschaer@gmail.com>
2015-01-26 17:09:29 +0100

    Add more unit tests

---
+++ wampspring/src/test/java/ch/rasc/wampspring/handler/WampMessageTypeMessageConditionTest.java
@@ -0,0 +1,113 @@
+package ch.rasc.wampspring.handler;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+import java.util.Collection;
+
+import org.junit.Test;
+
+import ch.rasc.wampspring.message.CallMessage;
+import ch.rasc.wampspring.message.PublishMessage;
+import ch.rasc.wampspring.message.SubscribeMessage;
+import ch.rasc.wampspring.message.UnsubscribeMessage;
+import ch.rasc.wampspring.message.WampMessageType;
+
+public class WampMessageTypeMessageConditionTest {
+
+	@Test
+	public void testGetMessageType() {
+		assertThat(WampMessageTypeMessageCondition.CALL.getMessageType()).isEqualTo(
+				WampMessageType.CALL);
+		assertThat(WampMessageTypeMessageCondition.PUBLISH.getMessageType()).isEqualTo(
+				WampMessageType.PUBLISH);
+		assertThat(WampMessageTypeMessageCondition.SUBSCRIBE.getMessageType()).isEqualTo(
+				WampMessageType.SUBSCRIBE);
+		assertThat(WampMessageTypeMessageCondition.UNSUBSCRIBE.getMessageType())
+				.isEqualTo(WampMessageType.UNSUBSCRIBE);
+
+		WampMessageTypeMessageCondition cond = new WampMessageTypeMessageCondition(
+				WampMessageType.CALL);
+		assertThat(cond.getMessageType()).isEqualTo(WampMessageType.CALL);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Test
+	public void testGetContent() {
+		assertThat(
+				(Collection<WampMessageType>) WampMessageTypeMessageCondition.CALL
+						.getContent()).hasSize(1).containsExactly(WampMessageType.CALL);
+
+		assertThat(
+				(Collection<WampMessageType>) WampMessageTypeMessageCondition.PUBLISH
+						.getContent()).hasSize(1)
+				.containsExactly(WampMessageType.PUBLISH);
+
+		assertThat(
+				(Collection<WampMessageType>) WampMessageTypeMessageCondition.SUBSCRIBE
+						.getContent()).hasSize(1).containsExactly(
+				WampMessageType.SUBSCRIBE);
+
+		assertThat(
+				(Collection<WampMessageType>) WampMessageTypeMessageCondition.UNSUBSCRIBE
+						.getContent()).hasSize(1).containsExactly(
+				WampMessageType.UNSUBSCRIBE);
+
+		WampMessageTypeMessageCondition cond = new WampMessageTypeMessageCondition(
+				WampMessageType.CALL);
+		assertThat((Collection<WampMessageType>) cond.getContent()).hasSize(1)
+				.containsExactly(WampMessageType.CALL);
+
+	}
+
+	@Test
+	public void testGetMatchingCondition() {
+		CallMessage cm = new CallMessage("1", "proc");
+		SubscribeMessage sm = new SubscribeMessage("proc");
+		UnsubscribeMessage um = new UnsubscribeMessage("proc");
+		PublishMessage pm = new PublishMessage("proc", "1");
+
+		assertThat(WampMessageTypeMessageCondition.CALL.getMatchingCondition(cm))
+				.isEqualTo(WampMessageTypeMessageCondition.CALL);
+		assertThat(WampMessageTypeMessageCondition.SUBSCRIBE.getMatchingCondition(cm))
+				.isNull();
+		assertThat(WampMessageTypeMessageCondition.UNSUBSCRIBE.getMatchingCondition(cm))
+				.isNull();
+		assertThat(WampMessageTypeMessageCondition.PUBLISH.getMatchingCondition(cm))
+				.isNull();
+
+		assertThat(WampMessageTypeMessageCondition.CALL.getMatchingCondition(sm))
+				.isNull();
+		assertThat(WampMessageTypeMessageCondition.SUBSCRIBE.getMatchingCondition(sm))
+				.isEqualTo(WampMessageTypeMessageCondition.SUBSCRIBE);
+		assertThat(WampMessageTypeMessageCondition.UNSUBSCRIBE.getMatchingCondition(sm))
+				.isNull();
+		assertThat(WampMessageTypeMessageCondition.PUBLISH.getMatchingCondition(sm))
+				.isNull();
+
+		assertThat(WampMessageTypeMessageCondition.CALL.getMatchingCondition(um))
+				.isNull();
+		assertThat(WampMessageTypeMessageCondition.SUBSCRIBE.getMatchingCondition(um))
+				.isNull();
+		assertThat(WampMessageTypeMessageCondition.UNSUBSCRIBE.getMatchingCondition(um))
+				.isEqualTo(WampMessageTypeMessageCondition.UNSUBSCRIBE);
+		assertThat(WampMessageTypeMessageCondition.PUBLISH.getMatchingCondition(um))
+				.isNull();
+
+		assertThat(WampMessageTypeMessageCondition.CALL.getMatchingCondition(pm))
+				.isNull();
+		assertThat(WampMessageTypeMessageCondition.SUBSCRIBE.getMatchingCondition(pm))
+				.isNull();
+		assertThat(WampMessageTypeMessageCondition.UNSUBSCRIBE.getMatchingCondition(pm))
+				.isNull();
+		assertThat(WampMessageTypeMessageCondition.PUBLISH.getMatchingCondition(pm))
+				.isEqualTo(WampMessageTypeMessageCondition.PUBLISH);
+
+		WampMessageTypeMessageCondition cond = new WampMessageTypeMessageCondition(
+				WampMessageType.CALL);
+		assertThat(cond.getMatchingCondition(cm)).isEqualTo(cond);
+		assertThat(cond.getMatchingCondition(sm)).isNull();
+		assertThat(cond.getMatchingCondition(um)).isNull();
+		assertThat(cond.getMatchingCondition(pm)).isNull();
+	}
+
+}
